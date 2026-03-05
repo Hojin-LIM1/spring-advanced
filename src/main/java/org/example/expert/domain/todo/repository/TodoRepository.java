3cc70db683deff91ed0fcb310c36b5c7fc722507
cherry pick <limhojin956@naver.com>
2026-03-05 13:49:23 +0900

    lv3 : n+1 문제 확인 후 수정(fetch join -> EntityGraph)

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,15 +15,20 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     //목록조회
     // todo entity 불러오기      // todo와 관련있는 유저정보 불러오고 수정시간 내림차순으로 정렬하기
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+
+    // "LEFT JOIN FETCH t.user u" fetch join문을 entityGraph로 변경 : user table 조회 후 Mapping
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt DESC")
     // page 형태로
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
 
     // 상세조회
     // todo entity 불러오기      // todo와 관련있는 유저정보 불러오는데 특정id에 관한것만 불러오기
+
+    // "LEFT JOIN FETCH t.user "  fetch join문을 entityGraph로 변경 : user table 조회 후 id Mapping
+    @EntityGraph(attributePaths = {"user"})
     @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
             "WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
