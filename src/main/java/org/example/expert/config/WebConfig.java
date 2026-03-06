fb1deb893cf16bbc20983f2aa0dae50ad2924d5c
cherry pick <limhojin956@naver.com>
2026-03-06 17:42:56 +0900

    LV5-2 : interceptor 작성 완료

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     //HttpServletRequest -> AuthUser -> @Auth AuthUser authUser로 전달하는 객체 생성
 
     private final AuthUserArgumentResolver authUserArgumentResolver;
+    private final AdminCheckInterceptor adminCheckInterceptor;
 
     /*
      * 4. 스프링이 컨트롤러의 파라미터를 처리할 때 사용할 '정보 리스트'에 커스텀 Resolver를 추가하는 작업
@@ -22,5 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserArgumentResolver);
+    }
+
+
+    //인터셉터 추가 /스프링이 제공하는 레지스트리 및 작성한 adminCheckInterceptor(admin 관련 주소 전부 가져오기)를 활용
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(adminCheckInterceptor).addPathPatterns("/admin/**");
+
+
     }
 }
