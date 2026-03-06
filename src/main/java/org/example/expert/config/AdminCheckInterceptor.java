3da08a98e97486219aa3e6ad11abecb62a3ffee5
cherry pick <limhojin956@naver.com>
2026-03-06 20:25:29 +0900

    Lv5-3 : 인터셉터 로깅 수정

--- src/main/java/org/example/expert/config/AdminCheckInterceptor.java
+++ src/main/java/org/example/expert/config/AdminCheckInterceptor.java
@@ -2,11 +2,14 @@ package org.example.expert.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.common.exception.InvalidRequestException;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import java.time.LocalDateTime;
 
-import javax.management.Attribute;
-
+@Slf4j
+@Component
 public class AdminCheckInterceptor implements HandlerInterceptor {
 
     @Override
@@ -21,6 +24,11 @@ public class AdminCheckInterceptor implements HandlerInterceptor {
         if (Role == null || !"Admin".equals(Role.toString())) {
             throw new InvalidRequestException(" 어드민 권한이 필요합니다.");
         }
+
+
+        //로깅 인포 추가!
+        log.info("인증된 관리자 접근, Time:{}, URL:{}",
+                LocalDateTime.now(), request.getRequestURL());
 
         // 특이사항 없으면 true
         return true;
